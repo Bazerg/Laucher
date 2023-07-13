8d8aa364e0a445b73b5223d88b852b843880afc9
Habdel <75091859+Bazerg@users.noreply.github.com>
2023-07-13 01:18:25 -0300

    init

---
+++ src/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CanaryLauncherUpdate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			App app = new App();
+			app.InitializeComponent();
+			app.Run();
+		}
+	}
+}
